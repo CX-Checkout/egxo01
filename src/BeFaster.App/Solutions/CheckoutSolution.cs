a5a3716d1807eeddf10ada5882755a6da3825356
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-27 17:05:33 +0100

    Fri Oct 27 17:05:33 BST 2017

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -44,7 +44,7 @@ namespace BeFaster.App.Solutions
                 return -1;
             }
             
-            var basketSkusCount
+            var basketSkusCount = basketSkus.
             
             return basketSkus
                 .GroupBy(sku => sku)
