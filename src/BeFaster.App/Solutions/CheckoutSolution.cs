6d90607bdf74031ec354044f8533e22b14405de1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-27 15:57:18 +0100

    Fri Oct 27 15:57:18 BST 2017

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -6,75 +6,67 @@ namespace BeFaster.App.Solutions
 {
     internal static class CheckoutSolution
     {
-        private static Dictionary<char, Dictionary<int, int)> {Egus = new Dictionary<char,
+        private static Dictionary<char, Dictionary<int, int>> items = new Dictionary<char, Dictionary<int, int>>
             {
-
-                3 ['A'] = new Dictionary<int, int)
-                5 {
-
-                : g [3] - 139,
-
-                : ; [1]=50
-
-                s b
-
-                ; ['B'] - new Dictionary<lnt, int)
-                3 {
-
-                E E [2] - 45,
-
-                2 E [1] - 39
-
-                s b
-
-                : ['C'] - new Dictionary<int, 1nt>
-                E {
-
-                a s m=2e
-
-                s L
-
-                t ['D'] = new Dictionary<int, int)
-                3 {
-
-                : E [1] - 15
-
-                E }
-
-        };
-        
-        public static int Checkout(string skus)
-        {
-            var availableSkus = items.Keys.ToList();
-
-            var basketSkus = skus.ToArray();
-            if (basketSkus.Any(sku => !availableSkus.Contains(sku)))
-            {
-                return -1;
-            }
-            
-            return basketSkus
-                .Group8y(sku => sku)
-                .Sum(g ->
-            {
-                var skuCheckoutSum = 0;
-                var skuPrices = items[g.Key];
-                var count = g.Count();
-                List(int) offers = skuPrices.Keys.0rderByDescending(k => k).ToList();
-                foreach (var offer in offers)
+                ['A'] = new Dictionary<int, int>
                 {
-                    if (offer > count)
-                        continue;
+                    [3] = 130,
+                    [1] = 50
+                },
+                ['B'] = new Dictionary<int, int>
+                {
+                    [2] = 45,
+                    [1] = 30
+                },
 
-                    var offerTimes = count / offer;
+                s b
+                : ['C'] - new Dictionary<int,
+                1nt>
+                E {
+                a s m = 2e
+                s L
+                t ['D'] = new Dictionary<int,
+                int
+            })
+            3;
 
-                    skuCheckoutSum += skuPrices[offer] * offerTimes;
-                    count = count - offer * offerTines;
-                }
-                return skuCheckoutSum;
-            });
+        {
+            : E[1] - 15
 
-        }        
+            E
+        }
+    };
+
+    public static int Checkout(string skus)
+    {
+    var availableSkus = items.Keys.ToList();
+
+    var basketSkus = skus.ToArray();
+        if (basketSkus.Any(sku => !availableSkus.Contains(sku)))
+    {
+        return -1;
     }
+    return basketSkus
+    .Group8y(sku => sku)
+    .Sum(g ->
+    {
+    var skuCheckoutSum = 0;
+    var skuPrices = items[g.Key];
+    var count = g.Count();
+    List(int) offers = skuPrices.Keys.0rderByDescending(k => k).ToList();
+        foreach (var offer in offers)
+    {
+        if (offer > count)
+            continue;
 
+        var offerTimes = count / offer;
+
+        skuCheckoutSum += skuPrices[offer] * offerTimes;
+        count = count - offer * offerTines;
+    }
+    return skuCheckoutSum;
+});
+
+}
+}
 }
