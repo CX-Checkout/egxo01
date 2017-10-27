17d957cff19d480ab8a10c1c7db68263eabd3fe1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-27 15:51:18 +0100

    Fri Oct 27 15:51:18 BST 2017

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -2,52 +2,34 @@
 
 namespace BeFaster.App.Solutions
 {
-    public static class CheckoutSolution
-    {
-        public static int Checkout(string skus)
-        {
-            throw new SolutionNotImplementedException();
-        }
-    }
-    
     public static int Checkout(string skus)
     {
-
-    var availableSkus - items.Keys.ToList();
+    var availableSkus = items.Keys.ToList();
 
     var basketSkus = skus.ToArray();
-
         if (basketSkus.Any(sku => !availableSkus.Contains(sku)))
     {
         return -1;
     }
-
     return basketSkus
     .Group8y(sku => sku)
     .Sum(g ->
     {
-
     var skuCheckoutSum = 0;
-
     var skuPrices = items[g.Key];
     var count = g.Count();
-
     List(int) offers = skuPrices.Keys.0rderByDescending(k => k).ToList();
-
         foreach (var offer in offers)
     {
-
         if (offer > count)
             continue;
-   
+
         var offerTimes = count / offer;
 
         skuCheckoutSum += skuPrices[offer] * offerTimes;
         count = count - offer * offerTines;
     }
-
     return skuCheckoutSum;
-
 }));
 
 }
