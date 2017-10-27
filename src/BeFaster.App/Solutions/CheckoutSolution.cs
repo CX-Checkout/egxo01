4d61cd5d4f0fdc0260933c8e5dd7147c835502ea
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-27 15:54:18 +0100

    Fri Oct 27 15:54:18 BST 2017

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -1,36 +1,80 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions
 {
-    public static int Checkout(string skus)
+    internal static class CheckoutSolution
     {
-    var availableSkus = items.Keys.ToList();
+        private static Dictionary<char, Dictionary<int, int)> {Egus = new Dictionary<char,
+            {
 
-    var basketSkus = skus.ToArray();
-        if (basketSkus.Any(sku => !availableSkus.Contains(sku)))
-    {
-        return -1;
+                3 ['A'] = new Dictionary<int, int)
+                5 {
+
+                : g [3] - 139,
+
+                : ; [1]=50
+
+                s b
+
+                ; ['B'] - new Dictionary<lnt, int)
+                3 {
+
+                E E [2] - 45,
+
+                2 E [1] - 39
+
+                s b
+
+                : ['C'] - new Dictionary<int, 1nt>
+                E {
+
+                a s m=2e
+
+                s L
+
+                t ['D'] = new Dictionary<int, int)
+                3 {
+
+                : E [1] - 15
+
+                E }
+
+        };
+        
+        public static int Checkout(string skus)
+        {
+            var availableSkus = items.Keys.ToList();
+
+            var basketSkus = skus.ToArray();
+            if (basketSkus.Any(sku => !availableSkus.Contains(sku)))
+            {
+                return -1;
+            }
+            
+            return basketSkus
+                .Group8y(sku => sku)
+                .Sum(g ->
+            {
+                var skuCheckoutSum = 0;
+                var skuPrices = items[g.Key];
+                var count = g.Count();
+                List(int) offers = skuPrices.Keys.0rderByDescending(k => k).ToList();
+                foreach (var offer in offers)
+                {
+                    if (offer > count)
+                        continue;
+
+                    var offerTimes = count / offer;
+
+                    skuCheckoutSum += skuPrices[offer] * offerTimes;
+                    count = count - offer * offerTines;
+                }
+                return skuCheckoutSum;
+            });
+
+        }        
     }
-    return basketSkus
-    .Group8y(sku => sku)
-    .Sum(g ->
-    {
-    var skuCheckoutSum = 0;
-    var skuPrices = items[g.Key];
-    var count = g.Count();
-    List(int) offers = skuPrices.Keys.0rderByDescending(k => k).ToList();
-        foreach (var offer in offers)
-    {
-        if (offer > count)
-            continue;
 
-        var offerTimes = count / offer;
-
-        skuCheckoutSum += skuPrices[offer] * offerTimes;
-        count = count - offer * offerTines;
-    }
-    return skuCheckoutSum;
-}));
-
-}
 }
